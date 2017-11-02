b269c21c112fc14bd4d657fcae35efa5ca502680
captain-yuca <manuel.baez@upr.edu>
2017-11-02 11:06:33 -0400

    Minor changes

--- mbWebsite/settings.py
+++ mbWebsite/settings.py
@@ -124,7 +124,7 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
@@ -149,4 +149,3 @@ EMAIL_PORT = 587
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 #
-DEBUG=True
